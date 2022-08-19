fc973e7a9b2c0bd06d1da147aa4f9a236031025c
James Mburu Githire <jamesmburug24@gmail.com>
2022-08-19 16:57:21 +0300

    Commit of core deliverables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 // write your code here
+function placeImg(obj){
+    const pin = document.createElement('img');
+    const divName = document.querySelector(".name");
+    const divRes = document.querySelector(".restaurant");
+    const divImg = document.querySelector(".detail-image");
+    const divRate = document.querySelector("#rating-display");
+    const divComment = document.querySelector("#comment-display");
+    pin.id= obj.name
+    pin.src=obj.image;
+    document.getElementById("ramen-menu").appendChild(pin)
+    pin.addEventListener("click",(e)=>{
+        divName.textContent = obj.name;
+        divRes.textContent = obj.restaurant
+        divImg.src = obj.image;
+        divRate.textContent = obj.rating;
+        divComment.textContent = obj.comment;
+    })
+}
+fetch("http://localhost:3000/ramens")
+    .then(resp => resp.json())
+    .then(obj => {
+        for (element of obj) {
+            console.log(element);
+            placeImg(element);
+        }
+    }
+    )
